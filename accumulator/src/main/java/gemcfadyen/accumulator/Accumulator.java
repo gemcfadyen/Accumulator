2db704067f34f45e9406a23a55b65620ef519416
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-01-12 13:00:57 +0000

    accumulator returns 1 if 1 is input

--- accumulator/src/main/java/gemcfadyen/accumulator/Accumulator.java
+++ accumulator/src/main/java/gemcfadyen/accumulator/Accumulator.java
@@ -3,6 +3,9 @@ package gemcfadyen.accumulator;
 public class Accumulator {
 
 	public int add(String numbers) {
+		if (numbers.equals("1")) {
+			return 1;
+		}
 		return 0;
 	}
 
--- accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
+++ accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
@@ -4,15 +4,27 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccumulatorTest {
+	private Accumulator accumulator;
+	
+	@Before
+	public void setup(){
+		accumulator = new Accumulator();
+	}
 	
 	@Test
 	public void shouldReturn0WhenEmptyStringIsPassedToAccumulator(){
-		Accumulator calculator = new Accumulator();
-		int sum = calculator.add("");
+		int sum = accumulator.add("");
 		assertThat(sum, is(equalTo(0)));
+	}
+	
+	@Test
+	public void shouldReturn1When1IsPassedToAccumulator(){
+		int sum=accumulator.add("1");
+		assertThat(sum, is(equalTo(1)));
 	}
 
 }
