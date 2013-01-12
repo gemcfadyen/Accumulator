34e34fd157312790a8b8f51976e150304d918d70
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-01-12 13:35:56 +0000

    updated to handle an unknown amount of numbers seperated by commas

--- accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
+++ accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
@@ -26,7 +26,9 @@ public class AccumulatorTest {
 	@Parameters
 	public static List<Object[]> testDataInitialisation() {
 		return Arrays.asList(new Object[][] { { "", 0 }, { "1", 1 },
-				{ "3", 3 }, { "10", 10 }, { "1,2", 3 } });
+				{ "3", 3 }, { "10", 10 }, { "1,2", 3 }, { "3,4", 7 },
+				{ "10,10", 20 }, { "1,2,3", 6 }, { "7,8,9,10", 34 },
+				{ "1,2,3,4,5,6,7", 28 } });
 	}
 
 	@Test
