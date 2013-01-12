1e456b0eff99e6cec4d124928069d9362aaf9d87
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-01-12 13:18:56 +0000

    refactored test to be a parameterized unit test

--- accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
+++ accumulator/src/test/java/gemcfadyen/accumulator/AccumulatorTest.java
@@ -4,39 +4,36 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class AccumulatorTest {
 	private Accumulator accumulator;
-	
-	@Before
-	public void setup(){
+	private String input;
+	private int sum;
+
+	public AccumulatorTest(String input, int sum) {
+		this.input = input;
+		this.sum = sum;
+	}
+
+	@Parameters
+	public static List<Object[]> testDataInitialisation() {
+		return Arrays.asList(new Object[][] { { "", 0 }, { "1", 1 },
+				{ "3", 3 }, { "10", 10 } });
+	}
+
+	@Test
+	public void shouldReturnTotalOfNumbersPassedIntoAccumulator() {
 		accumulator = new Accumulator();
-	}
-	
-	@Test
-	public void shouldReturn0WhenEmptyStringIsPassedToAccumulator(){
-		int sum = accumulator.add("");
-		assertThat(sum, is(equalTo(0)));
-	}
-	
-	@Test
-	public void shouldReturn1When1IsPassedToAccumulator(){
-		int sum=accumulator.add("1");
-		assertThat(sum, is(equalTo(1)));
-	}
-	
-	@Test
-	public void shouldReturn3When3IsPassedToAccumulator(){
-		int sum=accumulator.add("3");
-		assertThat(sum, is(equalTo(3)));
-	}
-	
-	@Test
-	public void shouldReturn10When10IsPassedIn(){
-		int sum = accumulator.add("10");
-		assertThat(sum, is(equalTo(10)));
+		int result = accumulator.add(input);
+		assertThat(result, is(equalTo(sum)));
 	}
 
 }
