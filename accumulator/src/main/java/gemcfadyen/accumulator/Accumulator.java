6e91c6f5fb522eeb4c0f0f1e001568121a618199
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-01-12 14:01:35 +0000

    updated to allow mixture of new line and comma delimeters

--- accumulator/src/main/java/gemcfadyen/accumulator/Accumulator.java
+++ accumulator/src/main/java/gemcfadyen/accumulator/Accumulator.java
@@ -4,28 +4,35 @@ import static java.lang.Integer.valueOf;
 
 public class Accumulator {
 
-	private static final String DELIMITER = ",";
-	private int result = 0;
+	private static final String COMMA_DELIMITER = ",";
+	private static final String NEWLINE_DELIMITER = "\n";
 
-	public int add(String input) {
-		if (noNumbersPresentIn(input)) {
-			return result;
+	public int add(String numbers) {
+		if (isBlank(numbers)) {
+			return 0;
 		} else {
-			String[] numbers = input.split(DELIMITER);
-			result = sum(numbers);
-			return result;
+			String inputSeperatedByCommas = putCommaInPlaceOfAllNewLineDelimetersIn(numbers);
+			return sum(getNumbersFrom(inputSeperatedByCommas));
 		}
+	}
 
+	private String[] getNumbersFrom(String input) {
+		return input.split(COMMA_DELIMITER);
+	}
+
+	private String putCommaInPlaceOfAllNewLineDelimetersIn(String input) {
+		return input.replace(NEWLINE_DELIMITER, COMMA_DELIMITER);
 	}
 
 	private int sum(String[] numbers) {
+		int result = 0;
 		for (String number : numbers) {
 			result = result + valueOf(number);
 		}
 		return result;
 	}
 
-	private boolean noNumbersPresentIn(String input) {
+	private boolean isBlank(String input) {
 		return input.length() == 0;
 	}
 
